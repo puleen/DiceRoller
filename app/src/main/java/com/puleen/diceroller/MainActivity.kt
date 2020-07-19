013c266787b641c63fbb7bf213f716590bdb4230
Puleen Patel <puleen@gmail.com>
2020-07-18 23:50:44 -0400

    Initial commit

---
+++ app/src/main/java/com/puleen/diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.puleen.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class qMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
